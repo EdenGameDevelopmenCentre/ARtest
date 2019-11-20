ebd6c669812c97b33546194bdc205c62b7090ac3
EGDC <edengamedevelopmentcentre@gmail.com>
2019-11-20 12:32:39 +1100

    Hide/Show working

--- JS.js
+++ JS.js
@@ -1,10 +1,25 @@
+var initialVarible = document.querySelector("#ducky")
+var badVarible = document.querySelector("#ducky")
+var goodVarible = document.querySelector("#ducky")
+
 function myFunctionMid(){
     alert("middle clicked");
+    
+    
+    console.log(initialVarible)
+    if (initialVarible.style.visiblity === "visible") {
+      initialVarible.setAttribute('visible',false);
+      console.log("if")
+    } else {
+      initialVarible.setAttribute('visible',true);
+      console.log("else")
+    }
+    console.log("button working")
     }
 
 function myFunctionRight(){
     alert("right clicked");
+
+    initialVarible.setAttribute('visible',false);
+    
     }
-
-
-
