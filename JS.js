a4b77c29c1e15d688d920243b448184b45600485
EGDC <edengamedevelopmentcentre@gmail.com>
2019-11-20 13:12:59 +1100

    1 showing at start, buttons show other ones

--- JS.js
+++ JS.js
@@ -1,18 +1,32 @@
 var initialVarible = document.querySelector("#ducky")
-var badVarible = document.querySelector("#ducky")
-var goodVarible = document.querySelector("#ducky")
+var badVarible = document.querySelector("#avocadoy")
+var goodVarible = document.querySelector("#brainstemy")
+
+
+function codeAddress() {
+  initialVarible.setAttribute('visible',true);
+  badVarible.setAttribute('visible',false);
+  goodVarible.setAttribute('visible',false);
+  console.log(goodVarible)
+  alert('ok');
+}
+window.onload = codeAddress;
 
 function myFunctionMid(){
     alert("middle clicked");
     
     
     console.log(initialVarible)
-    if (initialVarible.style.visiblity === "visible") {
+    if (initialVarible.getAttribute('visiblity') === 'true') {
       initialVarible.setAttribute('visible',false);
       console.log("if")
     } else {
-      initialVarible.setAttribute('visible',true);
+      initialVarible.setAttribute('visible',false);
       console.log("else")
+
+
+      badVarible.setAttribute('visible',true);
+      goodVarible.setAttribute('visible',false);
     }
     console.log("button working")
     }
@@ -21,5 +35,10 @@ function myFunctionRight(){
     alert("right clicked");
 
     initialVarible.setAttribute('visible',false);
-    
+    console.log(initialVarible)
+    badVarible.setAttribute('visible',false);
+    console.log(badVarible)
+    goodVarible.setAttribute('visible',true);
+    console.log(goodVarible)
+
     }
